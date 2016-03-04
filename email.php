daf13a889249ccc3d12c3829fbc52aa84c59092e
Stefano Franzin <stefano.franzin@seesaw.it>
2016-03-04 20:15:44 +0000

    Smtp mailer

--- email.php
+++ email.php
@@ -1,36 +1,55 @@
 <?php
 require "config.php";
+require 'class.phpmailer.php';
+require 'class.smtp.php';
 
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = $recipient_email;
-
         // Set the email subject.
         $subject = "Nuovo contatto da silviagalasso.it da $name";
 
         // Build the email content.
-        $email_content = "Nome: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Messaggio:\n$message\n";
+        $email_content = "";
+        $email_content .= "Nome: $name<br />";
+        $email_content .= "Email: $email<br />";
+        $email_content .= "Messaggio:<br />$message<br />";
 
         // Build the email headers.
-        $email_headers = "Date: $name <$email>";
+        
+        date_default_timezone_set('Etc/UTC');
+
+		$mail = new PHPMailer();
+
+		$mail->IsSMTP();
+		$mail->CharSet = 'UTF-8';
+
+		$mail->Host       = $smtp_host;
+		$mail->SMTPDebug  = 0;
+		$mail->SMTPAuth   = true;
+		$mail->Port       = $smtp_post;
+		$mail->Username   = $smtp_user;
+		$mail->Password   = $smtp_password;
+
+		$mail->setFrom($email);
+		$mail->addAddress($recipient_email);
+
+		$mail->isHTML(true);
+
+		$mail->Subject = $subject;
+		$mail->Body    = $email_content;
 
         // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
+       if ($mail->send()) {
+           http_response_code(200);
         } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
+           	http_response_code(500);
         }
 
     } else {
