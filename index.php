74bf1c22848f6aa00d417a5a66d7125414d414c8
Stefano Franzin <stefano.franzin@seesaw.it>
2016-03-26 10:57:30 +0000

    Portfolio descriptions

--- index.php
+++ index.php
@@ -195,18 +195,91 @@
 					<div class="row">
 						<div class="twelve columns">
 							<h3 class="black-box">portfolio</h3>
-							<p>Intro.</p>
 							<div class="portfolio owl-carousel owl-theme">
-								<div class="item"><a href="portfolio/portfolio-01.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-01-th.jpg" alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-02.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-02-th.jpg"  alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-03.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-03-th.jpg"  alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-04.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-04-th.jpg"  alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-05.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-05-th.jpg"  alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-06.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-06-th.jpg"  alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-07.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-07-th.jpg"  alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-08.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-08-th.jpg"  alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-09.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-09-th.jpg"  alt="Portfolio" /></a></div>
-								<div class="item"><a href="portfolio/portfolio-10.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-10-th.jpg"  alt="Portfolio" /></a></div>
+								<div class="item">
+									<a href="portfolio/portfolio-01.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-01-th.jpg" alt="Portfolio" /></a>
+									<div class="description">
+										<h5>progetto grafico / impaginazione / correzione bozze</h5>
+										<p>Leonardo Benevolo<br />
+										<strong><em>Indagine su Santo Spirito di Brunelleschi</em></strong><br />
+										Guaraldi editore (2015)</p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-02.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-02-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+										<h5>progetto grafico / impaginazione / correzione bozze</h5>
+										<p>G. Bordignon, M. Centanni, G. Cerri, S. Galasso, S. Garipoli, A. Grilli, C. Licitra, F. Lo Piparo, M. Nuzzi, A. Pedersoli, L. Rebaudo, O. Taplin<br />
+										<strong><em>Scene dal mito. Iconologia del dramma antico</em></strong><br />
+										Guaraldi editore (2015)</p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-03.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-03-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+										<h5>progetto grafico / impaginazione / correzione bozze</h5>
+										<p>Lorenzo Bonoldi<br />
+										<strong><em>Isabella d’Este. La Signora del Rinascimento</em></strong><br />
+										Guaraldi editore (2015)</p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-04.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-04-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+										<h5>progetto grafico / impaginazione</h5>
+										<p>Elisa Bastianello<br />
+										<strong><em>Il Castello e il Feudo dei Bissari tra storia e mito</em></strong><br />
+										edizioni Omega s.r.l. (2014)</p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-05.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-05-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+										<h5>graphic design</h5>
+										<p>Materiale pubblicitario:<br />
+										<strong><em>dépliant / brochure / volantini / biglietti da visita</em></strong><br />
+										</p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-06.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-06-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+										<h5>graphic design</h5>
+										<p>Prove di grafica pubblicitaria e riproduzione grafica di oggetti tridimensionali<br />
+										<span class="details">immagini prese da Internet a puro scopo esercitativo / copyright non verificato</span></p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-07.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-07-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+										<h5>graphic design</h5>
+										<p>Esercizi di fotoritocco / restauro fotografico digitale<br />
+										<span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</span>
+									</p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-08.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-08-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+										<h5>graphic design</h5>
+										<p>Prove di grafica pubblicitaria / fotoritocco<br />
+										<span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</span><br />
+										</p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-09.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-09-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+										<h5>graphic design</h5>
+										<p>Esercizi di grafica vettoriale<br />
+										<span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</p>
+									</div>
+								</div>
+								<div class="item">
+									<a href="portfolio/portfolio-10.jpg" data-lightbox="portfolio"><img src="portfolio/portfolio-10-th.jpg"  alt="Portfolio" /></a>
+									<div class="description">
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
