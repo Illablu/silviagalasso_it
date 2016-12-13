3b746fd53fe53e97dbf6dbe58cb48ff510b0076e
Stefano Franzin <stefano.franzin@gmail.com>
2016-12-13 00:04:18 +0000

    Hover status and click events

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,10 @@
 // mobile menu slide from the left
-$('[data-toggle="slide-collapse"]').on('click', function() {
+$('[data-toggle="slide-collapse"]').on('click', function(event) {
+	event.stopPropagation();
     $navMenuCont = $($(this).data('target'));
     $navMenuCont.animate({'width':'toggle'}, 350);
 })
-$('[data-toggle="slide-collapse"]').on('blur', function() {
-    $navMenuCont = $($(this).data('target'));
-    $navMenuCont.animate({'width':'toggle'}, 350);
+$('body').on('click', function(event) {
+    $navMenuCont = $($('[data-toggle="slide-collapse"]').data('target'));
+    $navMenuCont.animate({'width':'hide'}, 350);
 });
