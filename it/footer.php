fa455af6ee691b6e0a3d185f9cb602e4405e8d0f
Stefano Franzin <stefano.franzin@gmail.com>
2017-01-02 13:41:18 +0100

    Modal styles

--- it/footer.php
+++ it/footer.php
@@ -44,78 +44,79 @@
   </footer>
 
   <div id="immagine_coordinata" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="immagine_coordinata">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog" role="document">
       <div class="modal-content">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12">
-              <h5>immagine coordinata</h5>
-              <ul>
-                <li>insieme delle rappresentazioni visive di un’entita&agrave; (azienda, societ&agrave;, ente, associazione, attivit&agrave; commerciale) atte a renderla conosciuta e immediatamente riconoscibile da un determinato pubblico.</li>
-                <li>si compone di differenti elementi della comunicazione visiva (loghi, colori, caratteri tipografici, impaginazione) messi coerentemente in relazione tra loro in modo da fornire ai destinatari precisi punti di riferimento e indurre un processo di progressivo riconoscimento dell’azienda o del marchio e della sua attivit&agrave;, con conseguenze positive sul suo successo.</li>
-              </ul>      
-            </div>
-          </div>
+        <div class="modal-header">
+          <button type="button" class="close pull-right" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4>immagine coordinata</h4>
+        </div>
+        <div class="modal-body">
+          <ul>
+            <li>insieme delle rappresentazioni visive di un’entit&agrave; (azienda, societ&agrave;, ente, associazione, attivit&agrave; commerciale) atte a renderla conosciuta e immediatamente riconoscibile da un determinato pubblico.</li>
+            <li>si compone di differenti elementi della comunicazione visiva (loghi, colori, caratteri tipografici, impaginazione) messi coerentemente in relazione tra loro in modo da fornire ai destinatari precisi punti di riferimento e indurre un processo di progressivo riconoscimento dell’azienda o del marchio e della sua attivit&agrave;, con conseguenze positive sul suo successo.</li>
+          </ul>      
         </div>
       </div>
     </div>
   </div>
 
 <div id="company_profile" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="company_profile">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog" role="document">
       <div class="modal-content">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12">
-              <h5>company profile</h5>
-              <ul>
-                <li>strumento della comunicazione visiva atto a rispecchiare e valorizzare l’identit&agrave; di un’azienda.</li>
-                <li>graficamente si presenta come un d&egrave;pliant o una brochure di forte impatto visivo e di aspetto raffinato ed elegante, il cui scopo è presentare in maniera professionale la propria impresa al mercato di riferimento e alla clientela. </li>
-                <li>serve a esprimere:
-                chi sei | in che mercato e contesto operi | come immagini il tuo futuro | quali sono i valori su cui si fonda la tua azienda | quali sono i tuoi obiettivi.</li>
-                <li>la continuit&agrave; e la coerenza fra i contenuti è accentuata dall’ideazione di una storia, atta a fungere da fil rouge e coinvolgere emotivamente il lettore.</li>
-                <li>l'aspetto grafico risulta fondamentale nel risaltare i contenuti e impressionare positivamente il lettore, evidenziando
-                i concetti espressi, attirando la sua attenzione ed esercitando una forte ed efficace impressione comunicativa.</li>
-                <li>l'obiettivo è catturare l’attenzione del lettore o del potenziale cliente, stimolarne la curiosit&agrave;, impressionarlo positivamente, conquistarlo.</li>
-              </ul>      
-            </div>
-          </div>
+        <div class="modal-header">
+          <button type="button" class="close pull-right" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4>company profile</h4>
+        </div>
+        <div class="modal-body">
+          <ul>
+            <li>strumento della comunicazione visiva atto a rispecchiare e valorizzare l’identit&agrave; di un’azienda.</li>
+            <li>graficamente si presenta come un d&egrave;pliant o una brochure di forte impatto visivo e di aspetto raffinato ed elegante, il cui scopo è presentare in maniera professionale la propria impresa al mercato di riferimento e alla clientela. </li>
+            <li>serve a esprimere:
+            chi sei | in che mercato e contesto operi | come immagini il tuo futuro | quali sono i valori su cui si fonda la tua azienda | quali sono i tuoi obiettivi.</li>
+            <li>la continuit&agrave; e la coerenza fra i contenuti è accentuata dall’ideazione di una storia, atta a fungere da fil rouge e coinvolgere emotivamente il lettore.</li>
+            <li>l'aspetto grafico risulta fondamentale nel risaltare i contenuti e impressionare positivamente il lettore, evidenziando
+            i concetti espressi, attirando la sua attenzione ed esercitando una forte ed efficace impressione comunicativa.</li>
+            <li>l'obiettivo è catturare l’attenzione del lettore o del potenziale cliente, stimolarne la curiosit&agrave;, impressionarlo positivamente, conquistarlo.</li>
+          </ul>      
         </div>
       </div>
     </div>
   </div>
 
   <div id="libri" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="libri">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog" role="document">
       <div class="modal-content">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12">
-              <h5>libri, che passione!</h5>
-              <p>amo la carta e amo i libri, alcuni pi&ugrave; di altri... ve ne elenco qualcuno tra i miei preferiti! (rigorosamente in ordine sparso)</p>
-              <ul>
-                <li>Monocle Book Collection —Gestalten   A Map of the World —Gestalten</li>
-                <li>The Kinfolk Table —Ouur</li>
-                <li>The Kinfolk Home —Ouur</li>
-                <li>Designing News —Gestalten</li>
-                <li>Graphic Design. The New Basics —Princeton Architectural Press</li>
-                <li>Thinking with Type —Princeton Architectural Press “i want to make</li>
-                <li>Da cosa nasce cosa —Laterza   Graphic Design —Hoepli</li>
-                <li>Critica portatile al visual design —Einaudi</li>
-                <li>Logo Design Love —New Riders</li>
-                <li>Work for Money, Design for Love —New Riders   How to —Thames & Hudson</li>
-                <li>Logo Type —Laurence King</li>
-                <li>Type matters! —Merrell</li>
-                <li>Grid Systems in Graphic Design -Niggli</li>
-                <li>Il nuovo manuale del grafico -Zanichelli</li>
-                <li>The Design Book —Phaidon</li>
-                <li>la serie su Aristotele detective di Margareth Doody —Sellerio   la serie su 007 di Ian Fleming —Adelphi</li>
-                <li>La Storia del Mondo in 100 oggetti —Adelphi</li>
-                <li>La Grande Storia —National Geographic</li>
-                <li>tutto Sherlock Holmes —anche la serie tv!</li>
-              </ul>      
-            </div>
-          </div>
+        <div class="modal-header">
+          <button type="button" class="close pull-right" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4>libri, che passione!</h4>
+          <p>amo la carta e amo i <span class="highlight strong">libri</span>, alcuni pi&ugrave; di altri... ve ne elenco qualcuno tra i miei <span class="highlight strong">preferiti</span>! (rigorosamente in ordine sparso)</p>
+        </div>
+        <div class="modal-body">
+          <ul>
+            <li>Monocle Book Collection <span class="publisher">—Gestalten</span></li>
+            <li>A Map of the World <span class="publisher">—Gestalten</span></li>
+            <li>The Kinfolk Table <span class="publisher">—Ouur</span></li>
+            <li>The Kinfolk Home <span class="publisher">—Ouur</span></li>
+            <li>Designing News <span class="publisher">—Gestalten</span></li>
+            <li>Graphic Design. The New Basics <span class="publisher">—Princeton Architectural Press</span></li>
+            <li>Thinking with Type <span class="publisher">—Princeton Architectural Press</span></li>
+            <li>Da cosa nasce cosa <span class="publisher">—Laterza</span></li>
+            <li>Graphic Design <span class="publisher">—Hoepli</span></li>
+            <li>Critica portatile al visual design <span class="publisher">—Einaudi</span></li>
+            <li>Logo Design Love <span class="publisher">—New Riders</span></li>
+            <li>Work for Money, Design for Love <span class="publisher">—New Riders</span></li>
+            <li>How to <span class="publisher">—Thames & Hudson</span></li>
+            <li>Logo Type <span class="publisher">—Laurence King</span></li>
+            <li>Type matters! <span class="publisher">—Merrell</span></li>
+            <li>Grid Systems in Graphic Design -Niggli</span></li>
+            <li>Il nuovo manuale del grafico -Zanichelli</span></li>
+            <li>The Design Book <span class="publisher">—Phaidon</span></li>
+            <li>la serie su Aristotele detective di Margareth Doody <span class="publisher">—Sellerio</span></li>
+            <li>la serie su 007 di Ian Fleming <span class="publisher">—Adelphi</span></li>
+            <li>La Storia del Mondo in 100 oggetti <span class="publisher">—Adelphi</span></li>
+            <li>La Grande Storia <span class="publisher">—National Geographic</span></li>
+            <li>tutto Sherlock Holmes <span class="publisher">—anche la serie tv!</span></li>
+          </ul>      
         </div>
       </div>
     </div>
