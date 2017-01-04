dabedb87452ca6d7c9da8ffa43e088e62dfb19c1
Stefano Franzin <stefano.franzin@gmail.com>
2017-01-04 16:51:58 +0100

    Menu fix

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,9 @@
 $('[data-toggle="slide-collapse"]').on('click', function(event) {
 	event.stopPropagation();
     $navMenuCont = $($(this).data('target'));
-    $navMenuCont.animate({'width':'toggle', 'opacity' : 'toggle'}, 350);
+    $navMenuCont.animate({'width':'toggle', 'opacity' : '1'}, 350);
 })
 $('body').on('click', function(event) {
     $navMenuCont = $($('[data-toggle="slide-collapse"]').data('target'));
-    $navMenuCont.animate({'width':'hide', 'opacity' : 'toggle'}, 350);
+    $navMenuCont.animate({'width':'hide', 'opacity' : '0'}, 350);
 });
