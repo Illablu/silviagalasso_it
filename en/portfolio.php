3359aca61be6001d9cf624de961863ae2076f319
Stefano Franzin <stefano.franzin@gmail.com>
2017-02-20 22:27:43 +0000

    Portfolio

--- en/portfolio.php
+++ en/portfolio.php
@@ -8,90 +8,118 @@ include('header.php');
 
     <div class="portfolio owl-carousel owl-theme">
       <div class="item">
-        <a href="/portfolio/portfolio-01.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-01-th.jpg" alt="Portfolio" /></a>
+        <a href="/portfolio/portfolio-01.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-01-th.jpg" alt="Silvia Galasso | Portfolio | editorial design" /></a>
         <div class="description">
-          <h5>progetto grafico / impaginazione / correzione bozze</h5>
-          <p>Leonardo Benevolo<br />
-          <strong><em>Indagine su Santo Spirito di Brunelleschi</em></strong><br />
-          Guaraldi editore (2015)</p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-02.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-02-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>progetto grafico / impaginazione / correzione bozze</h5>
-          <p>G. Bordignon, M. Centanni, G. Cerri, S. Galasso, S. Garipoli, A. Grilli, C. Licitra, F. Lo Piparo, M. Nuzzi, A. Pedersoli, L. Rebaudo, O. Taplin<br />
-          <strong><em>Scene dal mito. Iconologia del dramma antico</em></strong><br />
-          Guaraldi editore (2015)</p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-03.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-03-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>progetto grafico / impaginazione / correzione bozze</h5>
-          <p>Lorenzo Bonoldi<br />
-          <strong><em>Isabella d’Este. La Signora del Rinascimento</em></strong><br />
-          Guaraldi editore (2015)</p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-04.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-04-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>progetto grafico / impaginazione</h5>
-          <p>Elisa Bastianello<br />
-          <strong><em>Il Castello e il Feudo dei Bissari tra storia e mito</em></strong><br />
-          edizioni Omega s.r.l. (2014)</p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-05.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-05-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>graphic design</h5>
-          <p>Materiale pubblicitario:<br />
-          <strong><em>dépliant / brochure / volantini / biglietti da visita</em></strong><br />
+          <h5>editorial design: <br class="hidden-sm-up" />
+          layout <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          proofreading
+          </h5>
+          <p>
+            Leonardo Benevolo<br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            <strong><em>Indagine su Santo Spirito di Brunelleschi</em></strong><br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            Guaraldi editore (2015)
           </p>
         </div>
       </div>
       <div class="item">
-        <a href="/portfolio/portfolio-06.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-06-th.jpg"  alt="Portfolio" /></a>
+        <a href="/portfolio/portfolio-02.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-02-th.jpg" alt="Silvia Galasso | Portfolio | editorial design" /></a>
         <div class="description">
-          <h5>graphic design</h5>
-          <p>Prove di grafica pubblicitaria e riproduzione grafica di oggetti tridimensionali<br />
-          <span class="details">immagini prese da Internet a puro scopo esercitativo / copyright non verificato</span></p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-07.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-07-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>graphic design</h5>
-          <p>Prove di grafica pubblicitaria e riproduzione grafica di oggetti tridimensionali<br />
-          <span class="details">immagini prese da Internet a puro scopo esercitativo / copyright non verificato</span></p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-08.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-08-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>graphic design</h5>
-          <p>Esercizi di fotoritocco / restauro fotografico digitale<br />
-          <span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</span>
-        </p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-09.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-09-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>graphic design</h5>
-          <p>Prove di grafica pubblicitaria / fotoritocco<br />
-          <span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</span><br />
+          <h5>editorial design: <br class="hidden-sm-up" />
+          layout <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          proofreading</h5>
+          <p>
+            Aa. Vv.<br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            <strong><em>Scene dal mito. Iconologia del dramma antico</em></strong><br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            Guaraldi editore (2015)
           </p>
         </div>
       </div>
       <div class="item">
-        <a href="/portfolio/portfolio-10.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-10-th.jpg"  alt="Portfolio" /></a>
+        <a href="/portfolio/portfolio-03.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-03-th.jpg"  alt="Silvia Galasso | Portfolio | editorial design" /></a>
         <div class="description">
-          <h5>graphic design</h5>
-          <p>Esercizi di grafica vettoriale<br />
-          <span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</p>
+          <h5>editorial design: <br class="hidden-sm-up" />
+          layout <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          proofreading</h5>
+          <p>
+            Lorenzo Bonoldi<br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            <strong><em>Isabella d’Este. La Signora del Rinascimento</em></strong><br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            Guaraldi editore (2015)
+          </p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-04.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-04-th.jpg"  alt="Silvia Galasso | Portfolio | editorial design" /></a>
+        <div class="description">
+          <h5>editorial design: <br class="hidden-sm-up" />
+          layout</h5>
+          <p>
+            Elisa Bastianello<br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            <strong><em>Il Castello e il Feudo dei Bissari tra storia e mito</em></strong><br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            edizioni Omega s.r.l. (2014)
+          </p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-05.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-05-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+          photo editing  <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          digital photograph restoration
+          </h5>
+          <p>
+            original source is not proprietary content
+          </p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-06.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-06-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+            advertising graphics <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span> 
+            photo editing
+          </h5>
+          <p>
+            original source is not proprietary content
+          </p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-11.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-11-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+          advertising graphics <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span> photo editing
+          </h5>
+          <p>original source is not proprietary content</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-07.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-07-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+          advertising graphics <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span> photo editing and layout</h5>
+          <p>original source is not proprietary content</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-08.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-08-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />vectors design</h5>
+          <p>original source is not proprietary content</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-09.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-09-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />vectors design</h5>
+          <p>original source is not proprietary content</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-10.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-10-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+          image tracing</h5>
+          <p>original source is not proprietary content</p>
         </div>
       </div>
     </div>
--- it/portfolio.php
+++ it/portfolio.php
@@ -8,90 +8,116 @@ include('header.php');
 
     <div class="portfolio owl-carousel owl-theme">
       <div class="item">
-        <a href="/portfolio/portfolio-01.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-01-th.jpg" alt="Portfolio" /></a>
+        <a href="/portfolio/portfolio-01.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-01-th.jpg" alt="Silvia Galasso | Portfolio | grafica editoriale" /></a>
         <div class="description">
-          <h5>progetto grafico / impaginazione / correzione bozze</h5>
-          <p>Leonardo Benevolo<br />
-          <strong><em>Indagine su Santo Spirito di Brunelleschi</em></strong><br />
-          Guaraldi editore (2015)</p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-02.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-02-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>progetto grafico / impaginazione / correzione bozze</h5>
-          <p>G. Bordignon, M. Centanni, G. Cerri, S. Galasso, S. Garipoli, A. Grilli, C. Licitra, F. Lo Piparo, M. Nuzzi, A. Pedersoli, L. Rebaudo, O. Taplin<br />
-          <strong><em>Scene dal mito. Iconologia del dramma antico</em></strong><br />
-          Guaraldi editore (2015)</p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-03.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-03-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>progetto grafico / impaginazione / correzione bozze</h5>
-          <p>Lorenzo Bonoldi<br />
-          <strong><em>Isabella d’Este. La Signora del Rinascimento</em></strong><br />
-          Guaraldi editore (2015)</p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-04.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-04-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>progetto grafico / impaginazione</h5>
-          <p>Elisa Bastianello<br />
-          <strong><em>Il Castello e il Feudo dei Bissari tra storia e mito</em></strong><br />
-          edizioni Omega s.r.l. (2014)</p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-05.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-05-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>graphic design</h5>
-          <p>Materiale pubblicitario:<br />
-          <strong><em>dépliant / brochure / volantini / biglietti da visita</em></strong><br />
+          <h5>grafica editoriale: <br class="hidden-sm-up" />
+          progetto grafico <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          impaginazione <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          correzione bozze
+          </h5>
+          <p>
+            Leonardo Benevolo<br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            <strong><em>Indagine su Santo Spirito di Brunelleschi</em></strong><br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            Guaraldi editore (2015)
           </p>
         </div>
       </div>
       <div class="item">
-        <a href="/portfolio/portfolio-06.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-06-th.jpg"  alt="Portfolio" /></a>
+        <a href="/portfolio/portfolio-02.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-02-th.jpg" alt="Silvia Galasso | Portfolio | grafica editoriale" /></a>
         <div class="description">
-          <h5>graphic design</h5>
-          <p>Prove di grafica pubblicitaria e riproduzione grafica di oggetti tridimensionali<br />
-          <span class="details">immagini prese da Internet a puro scopo esercitativo / copyright non verificato</span></p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-07.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-07-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>graphic design</h5>
-          <p>Prove di grafica pubblicitaria e riproduzione grafica di oggetti tridimensionali<br />
-          <span class="details">immagini prese da Internet a puro scopo esercitativo / copyright non verificato</span></p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-08.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-08-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>graphic design</h5>
-          <p>Esercizi di fotoritocco / restauro fotografico digitale<br />
-          <span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</span>
-        </p>
-        </div>
-      </div>
-      <div class="item">
-        <a href="/portfolio/portfolio-09.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-09-th.jpg"  alt="Portfolio" /></a>
-        <div class="description">
-          <h5>graphic design</h5>
-          <p>Prove di grafica pubblicitaria / fotoritocco<br />
-          <span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</span><br />
+          <h5>grafica editoriale: <br class="hidden-sm-up" />
+          progetto grafico <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          impaginazione <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          correzione bozze</h5>
+          <p>
+            Aa. Vv.<br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            <strong><em>Scene dal mito. Iconologia del dramma antico</em></strong><br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            Guaraldi editore (2015)
           </p>
         </div>
       </div>
       <div class="item">
-        <a href="/portfolio/portfolio-10.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-10-th.jpg"  alt="Portfolio" /></a>
+        <a href="/portfolio/portfolio-03.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-03-th.jpg"  alt="Silvia Galasso | Portfolio | grafica editoriale" /></a>
         <div class="description">
-          <h5>graphic design</h5>
-          <p>Esercizi di grafica vettoriale<br />
-          <span class="details">immagine presa da Internet a puro scopo esercitativo / copyright non verificato</p>
+          <h5>grafica editoriale: <br class="hidden-sm-up" />
+          progetto grafico <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          impaginazione <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          correzione bozze</h5>
+          <p>
+            Lorenzo Bonoldi<br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            <strong><em>Isabella d’Este. La Signora del Rinascimento</em></strong><br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            Guaraldi editore (2015)
+          </p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-04.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-04-th.jpg"  alt="Silvia Galasso | Portfolio | grafica editoriale" /></a>
+        <div class="description">
+          <h5>grafica editoriale: <br class="hidden-sm-up" />
+          progetto grafico <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          impaginazione</h5>
+          <p>
+            Elisa Bastianello<br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            <strong><em>Il Castello e il Feudo dei Bissari tra storia e mito</em></strong><br class="hidden-sm-up" /><span class="hidden-xs-down">,</span>
+            edizioni Omega s.r.l. (2014)
+          </p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-05.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-05-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+          esercizi di fotoritocco <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span>
+          restauro fotografico digitale
+          </h5>
+          <p>immagine presa da Internet a puro scopo esercitativo / copyright non verificato</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-06.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-06-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+            prove di grafica pubblicitaria <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span> fotoritocco
+          </h5>
+          <p>immagini prese da Internet a puro scopo esercitativo / copyright non verificato</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-11.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-11-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+          prove di grafica pubblicitaria <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span> fotoritocco</h5>
+          <p>immagine presa da Internet a puro scopo esercitativo / copyright non verificato</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-07.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-07-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+          esercizi di grafica pubblicitaria <br class="hidden-sm-up" /><span class="hidden-xs-down">|</span> fotoritocco e impaginazione</h5>
+          <p>immagini prese da Internet a puro scopo esercitativo / copyright non verificato</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-08.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-08-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />riproduzione grafica di oggetti tridimensionali</h5>
+          <p>immagine presa da Internet a puro scopo esercitativo / copyright non verificato</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-09.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-09-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" /> riproduzione grafica di oggetti tridimensionali</h5>
+          <p>immagine presa da Internet a puro scopo esercitativo / copyright non verificato</p>
+        </div>
+      </div>
+      <div class="item">
+        <a href="/portfolio/portfolio-10.jpg" class="swipebox" rel="portfolio" ><img src="/portfolio/portfolio-10-th.jpg"  alt="Silvia Galasso | Portfolio | graphic design" /></a>
+        <div class="description">
+          <h5>graphic design: <br class="hidden-sm-up" />
+          esercizi di grafica vettoriale</h5>
+          <p>immagine presa da Internet a puro scopo esercitativo / copyright non verificato</p>
         </div>
       </div>
     </div>
