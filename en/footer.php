cfabc38fa3c448a23df09d518facae67d0a7592d
Stefano Franzin <stefano.franzin@gmail.com>
2017-02-20 18:37:56 +0000

    missing link

--- en/footer.php
+++ en/footer.php
@@ -139,7 +139,6 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
   <script src='/js/jquery-validate.min.js'></script>
-  <script src='/js/jquery-validate.messages_en.js'></script>
   <script src='https://www.google.com/recaptcha/api.js'></script>
   <script src="/js/menu.js" type="text/javascript"></script>
   <script src="/js/svg4everybody.min.js"></script>
